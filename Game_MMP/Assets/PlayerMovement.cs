5a1b5f513810341dad9f20bdcc952824417db1ca
marto97 <martinyankow@gmail.com>
2021-06-23 16:31:34 +0200

    added Jump(with SPACE)

--- Game_MMP/Assets/PlayerMovement.cs
+++ Game_MMP/Assets/PlayerMovement.cs
@@ -10,15 +10,23 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontalMove = 0f;
 
+    bool jump = false;
+
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true; 
+        }
     }
 
     void FixedUpdate ()
     {
         //Move Character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
