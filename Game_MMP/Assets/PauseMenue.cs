4ef655854f6d479dda15ac9e7ce508899c29d961
Luca Woik <lucaleon.woik@gmail.com>
2021-06-27 17:41:42 +0200

    TASK: Added pause button to level1 and logic to the pause menue

--- Game_MMP/Assets/PauseMenue.cs
+++ Game_MMP/Assets/PauseMenue.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenue : MonoBehaviour
 {
-    
+     public void MainMenue ()
+    {
+        SceneManager.LoadScene("MainMenue");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
