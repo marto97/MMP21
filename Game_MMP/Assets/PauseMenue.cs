8cb1fea4b3d2ecf8815e1a7b502a605f1f254712
Luca Woik <lucaleon.woik@gmail.com>
2021-06-28 22:32:55 +0200

    TASK: implemented time stop when game is paused, pauseMenue can be accessed via esc key

--- Game_MMP/Assets/PauseMenue.cs
+++ Game_MMP/Assets/PauseMenue.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenue : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenueUI;
+    public GameObject PauseOverlay;
+
      public void MainMenue ()
     {
         SceneManager.LoadScene("MainMenue");
@@ -14,5 +19,39 @@ public class PauseMenue : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    // called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+        
+    }
+
+    public void Resume ()
+    {
+        PauseMenueUI.SetActive(false);
+        PauseOverlay.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause ()
+    {
+        PauseMenueUI.SetActive(true);
+        PauseOverlay.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
